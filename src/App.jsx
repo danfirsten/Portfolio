70613ccbfb551b3de096cea32eb9515467318edd
danfirsten <dan.first.4444@gmail.com>
2024-07-23 09:17:20 -0700

    Scroll Snap + Global CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import "./app.scss"
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section><a href="">Hero</a></section>
+    <section>Parallax</section>
+    <section>Services</section>
+    <section>Parallax</section>
+    <section>Portfolio 1</section>
+    <section>Portfolio 2</section>
+    <section>Portfolio 3</section>
+    <section>Contact</section>
+  </div>;
 };
 
 export default App;
